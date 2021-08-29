85739d9d92442a9878dc5f588fdaccb20f43376b
Jhilam2014 <jhilambera@Jhilams-MacBook-Pro.local>
2021-08-30 00:02:21 +0530

    focuser camera

--- src/tele_robo/src/script/rpi_focuser.py
+++ src/tele_robo/src/script/rpi_focuser.py
@@ -8,7 +8,7 @@ import logging
 import socketserver
 from threading import Condition
 from http import server
-
+from flask import Flask
 PAGE="""\
     html>
         <head>
@@ -83,15 +83,22 @@ class StreamingServer(socketserver.ThreadingMixIn, server.HTTPServer):
     allow_reuse_address = True
     daemon_threads = True
 
-with picamera.PiCamera(resolution='640x480', framerate=24) as camera:
-    output = StreamingOutput()
-    camera.rotation = 90
-    camera.iso = 800
-    camera.shutter_speed = 200*10**3
-    camera.start_recording(output, format='mjpeg')
+camera = picamera.PiCamera(resolution='640x480', framerate=24)
+
+output = StreamingOutput()
+camera.rotation = 90
+camera.iso = 800
+camera.shutter_speed = 200*10**3
+camera.start_recording(output, format='mjpeg')
+app = Flask(__name__)
+
+@app.route('/')
+def run():
     try:
         address = ('', 8000)
         server = StreamingServer(address, StreamingHandler)
         server.serve_forever()
     finally:
         camera.stop_recording()
+if __name__ == '__main__':
+   app.run()
