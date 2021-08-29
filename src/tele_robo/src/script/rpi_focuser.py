21206809bd077dcf03fcd42fa5aada809121751c
Jhilam2014 <jhilambera@Jhilams-MacBook-Pro.local>
2021-08-29 21:47:20 +0530

    focuser camera

--- src/tele_robo/src/script/rpi_focuser.py
+++ src/tele_robo/src/script/rpi_focuser.py
@@ -83,6 +83,8 @@ with picamera.PiCamera(resolution='640x480', framerate=24) as camera:
     output = StreamingOutput()
     #Uncomment the next line to change your Pi's Camera rotation (in degrees)
     #camera.rotation = 90
+    camera.iso = 800
+    camera.shutter_speed = 20*10**3
     camera.start_recording(output, format='mjpeg')
     try:
         address = ('', 8000)
