99d1d9daaa5d608f65e57172f7ba4f2f115eba4a
Jhilam2014 <jhilambera@Jhilams-MacBook-Pro.local>
2021-09-01 01:08:30 +0530

    focuser camera server -1

--- src/tele_robo/src/script/flaskVideoStreaming/camera_pi.py
+++ src/tele_robo/src/script/flaskVideoStreaming/camera_pi.py
@@ -10,7 +10,7 @@ class Camera(BaseCamera):
         with picamera.PiCamera() as camera:
             # let camera warm up
             time.sleep(2)
-            camera.shutter_speed = 20000
+            camera.shutter_speed = 200000
             camera.iso = 800
             stream = io.BytesIO()
             for _ in camera.capture_continuous(stream, 'jpeg',
