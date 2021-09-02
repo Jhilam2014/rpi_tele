8e9c34b4bf55811b4d25b978956dda5245ca9867
Jhilam2014 <jhilambera@Jhilams-MacBook-Pro.local>
2021-09-02 20:31:44 +0530

    focuser camera server -1

--- src/tele_robo/src/script/flaskVideoStreaming/camera_pi.py
+++ src/tele_robo/src/script/flaskVideoStreaming/camera_pi.py
@@ -15,21 +15,15 @@ class Camera(BaseCamera):
             camera.iso = 800
             camera.annotate_text = '+'
 
-            a = np.zeros((400, 640, 3), dtype=np.uint8)
-            a[360, :, :] = 0xff
-            a[:, 400, :] = 0xff
-
-            o = camera.add_overlay(a.tobytes(),format='rgba',fullscreen = False)
-            o.alpha=64
-            o.layer=3
-
+          
 
 
             camera.framerate = 24
             stream = io.BytesIO()
-            for _ in camera.capture_continuous(stream, 'png',
-                                                 use_video_port=True):
+            for frame in camera.capture_continuous(stream, format="bgr",use_video_port=True):
                 # return current frame
+                image = frame.array
+                print(image)
                 stream.seek(0)
                 yield stream.read()
 
