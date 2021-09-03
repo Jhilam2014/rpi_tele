1f91a852c4a5591c9b0a806c9b4c2bdde0aca47e
Jhilam2014 <jhilambera@Jhilams-MacBook-Pro.local>
2021-09-04 00:19:59 +0530

    focuser camera server -1

--- src/tele_robo/src/script/flaskVideoStreaming/static/index.js
+++ src/tele_robo/src/script/flaskVideoStreaming/static/index.js
@@ -75,7 +75,7 @@
 //   ctx.stroke();
 // }
 
-document.getElementById("canvas").addEventListener("click", function(e){
+$("#canvas").addEventListener("click", function(e){
   var xPos = e.pageX - this.offsetLeft, 
       yPos = e.pageY - this.offsetTop;
       console.log(xPos,yPos);
@@ -88,3 +88,4 @@ function addCrossHair(x,y){
   .css("width", 30);
   container.appendChild(img);
 }
+
