fdbc2630b504a33afae0820b2bdb39f28a1124b7
Jhilam2014 <jhilambera@Jhilams-MacBook-Pro.local>
2021-09-09 20:44:56 +0530

    focuser cam settings update

--- src/tele_robo/src/script/flaskVideoStreaming/camera_pi.py
+++ src/tele_robo/src/script/flaskVideoStreaming/camera_pi.py
@@ -13,7 +13,6 @@ class Camera(BaseCamera):
             camera.shutter_speed = 6*10**5
             
             camera.iso = 800
-            camera.analog_gain = 12.0
             camera.awb_mode = "off"
             camera.drc = 'high'
             camera.framerate = 6
