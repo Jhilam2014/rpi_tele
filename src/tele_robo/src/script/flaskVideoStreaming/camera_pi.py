a0a3408d4049a5c29b49eb445e3c960c575a82fd
Jhilam2014 <jhilambera@Jhilams-MacBook-Pro.local>
2021-09-09 20:50:29 +0530

    focuser cam settings update

--- src/tele_robo/src/script/flaskVideoStreaming/camera_pi.py
+++ src/tele_robo/src/script/flaskVideoStreaming/camera_pi.py
@@ -1,23 +1,23 @@
 import io
 import time
 import picamera
+from fractions import Fraction
 from base_camera import BaseCamera
 import numpy as np
 
 class Camera(BaseCamera):
     @staticmethod
     def frames():
-        with picamera.PiCamera() as camera:
+        with picamera.PiCamera(resolution=(4056, 3040), framerate=Fraction(1, 6)) as camera:
             # let camera warm up
             camera.resolution = (640, 480)
             camera.shutter_speed = 6*10**5
             
             camera.iso = 800
             camera.awb_mode = "off"
-            camera.drc = 'high'
             camera.framerate = 6
             time.sleep(10)
-            camera.exposure_mode = 'off'
+            # camera.exposure_mode = 'off'
             stream = io.BytesIO()
             for _ in camera.capture_continuous(stream, 'jpeg',
                                                  use_video_port=True):
