5dd8b01ec700e8318087bcec3ae3c5f95c97dff7
Jhilam2014 <jhilambera@Jhilams-MacBook-Pro.local>
2021-09-23 23:19:35 +0530

    new viewport

--- src/tele_robo/src/script/flaskVideoStreaming/camera_pi.py
+++ src/tele_robo/src/script/flaskVideoStreaming/camera_pi.py
@@ -9,14 +9,14 @@ class Camera(BaseCamera):
     @staticmethod
     def frames(**parameters):
         shutterSpeed = parameters.get('shutter_speed',5)
-        frameRate = parameters.get('frame_rate',1/9)
+        frameRate = parameters.get('frame_rate',9)
         with picamera.PiCamera() as camera:
             print ('<==',shutterSpeed,frameRate)
             camera.resolution = (1920, 1080)
             camera.shutter_speed = 10*10**int(shutterSpeed)
             camera.iso = 800
             camera.awb_mode = "off"
-            camera.framerate = frameRate
+            camera.framerate = 1/int(frameRate)
             camera.awb_gains = 2.0
             time.sleep(2)
             # camera.exposure_mode = 'off'
